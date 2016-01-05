98651f9055dedaf7ebef4538dff3dafde949f465
annrayc <anna.raycay@gmail.com>
2016-01-06 00:52:53 +1000

    initial commit: new rails web app created using ruby 1.9.3-p194 and rails 3.2.18

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recruitment::Application.config.secret_token = '15f5990aba8124ca6ef6659f4d0d9e5a1eeae883dd997b82f00b389a8c967e25c1441787262afa3e23e952ff2a668d5a8e55c4ce4692b5701b8ba689e753acbf'
